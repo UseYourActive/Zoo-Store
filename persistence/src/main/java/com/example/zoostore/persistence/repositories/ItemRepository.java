dd0d99dcbca58dfa472825d3ce51d5986ae48d15
Alex Orozov <alexorozov@gmail.com>
2023-07-20 13:03:29 +0300

    Adding some functions in the repositories.

--- persistence/src/main/java/com/example/zoostore/persistence/repositories/ItemRepository.java
+++ persistence/src/main/java/com/example/zoostore/persistence/repositories/ItemRepository.java
@@ -1,11 +1,14 @@
 package com.example.zoostore.persistence.repositories;
 
 import com.example.zoostore.persistence.entities.Item;
+import com.example.zoostore.persistence.entities.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<Item, UUID> {
     Optional<Item> findItemById(UUID id);
+    List<Item> findItemsByProductName(String productName);
 }
--- persistence/src/main/java/com/example/zoostore/persistence/repositories/VendorRepository.java
+++ persistence/src/main/java/com/example/zoostore/persistence/repositories/VendorRepository.java
@@ -3,6 +3,7 @@ package com.example.zoostore.persistence.repositories;
 import com.example.zoostore.persistence.entities.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -10,6 +11,6 @@ import java.util.UUID;
 public interface VendorRepository  extends JpaRepository<Vendor, UUID> {
     Optional<Vendor> findVendorById(UUID id);
     Set<Vendor> findVendorsByIdIn(Set<UUID> id);
-    Optional<Vendor> findVendorByName(String name);
-    Optional<Vendor> findVendorByPhone(String phone);
+    List<Vendor> findVendorsByName(String name);
+    Optional<Vendor> findVendorsByPhone(String phone);
 }

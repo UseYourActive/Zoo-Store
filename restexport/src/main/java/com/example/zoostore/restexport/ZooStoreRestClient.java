7927ab2861f7237e5f1bef392854020d371d29fd
Alex Orozov <alexorozov@gmail.com>
2023-07-28 13:55:53 +0300

    WIP

--- restexport/src/main/java/com/example/zoostore/restexport/ZooStoreRestClient.java
+++ restexport/src/main/java/com/example/zoostore/restexport/ZooStoreRestClient.java
@@ -1,6 +1,5 @@
 package com.example.zoostore.restexport;
 
-import com.example.zoostore.api.operations.item.findall.FindAllItemsResult;
 import com.example.zoostore.api.operations.item.findbyid.FindItemByIdResponse;
 import feign.Headers;
 import feign.Param;
@@ -15,9 +14,9 @@ public interface ZooStoreRestClient {
     @RequestLine("GET /items/{request}")
     FindItemByIdResponse getItemById(@Param String request);
 
-    @RequestLine("GET /items/find-all?includeArchived={includeArchived}&pageNumber={pageNumber}&numberOfItemsPerPage={numberOfItemsPerPage}&tagId={tagId}")
-    FindAllItemsResult getAllItems(@Param(value = "includeArchived") Boolean includeArchived,
-                                   @Param(value = "pageNumber") Integer pageNumber,
-                                   @Param(value = "numberOfItemsPerPage") Integer numberOfItemsPerPage,
-                                   @Param(value = "tagId") @org.hibernate.validator.constraints.UUID String tagId);
+//    @RequestLine("GET /items/find-all?includeArchived={includeArchived}&pageNumber={pageNumber}&numberOfItemsPerPage={numberOfItemsPerPage}&tagId={tagId}")
+//    FindAllItemsResult getAllItems(@Param(value = "includeArchived") Boolean includeArchived,
+//                                   @Param(value = "pageNumber") Integer pageNumber,
+//                                   @Param(value = "numberOfItemsPerPage") Integer numberOfItemsPerPage,
+//                                   @Param(value = "tagId") String tagId);
 }
